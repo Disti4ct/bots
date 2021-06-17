9e8d12cfa3b7cfb05a64aa6a80eb5ef416167319
NotEternal <cent1pede@protonmail.ch>
2021-06-17 02:21:35 -0500

    Deleted pin message methods. Added user statuses. Added a poll about user deletion

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import time
 import config
 import logging
 import bot_helpers
@@ -23,7 +24,7 @@ async def on_user_joined(message: types.Message):
 		first_name = message.from_user.first_name
 
 		await message.bot.send_message(
-				message.chat.id,
+				config.GROUP_ID,
 				f"👋😊 Welcome *{first_name}* to our group",
 				"MarkdownV2",
 			)
@@ -37,7 +38,7 @@ async def on_user_left(message: types.Message):
 
 		db.delete_user(message.from_user.id)
 		await message.bot.send_message(
-				message.chat.id,
+				config.GROUP_ID,
 				f"🥺 Goodbuy *{first_name}*",
 				"MarkdownV2",
 			)
@@ -49,9 +50,9 @@ async def user_ban(message: types.Message):
 				await message.reply("⚠️ This command must be the answer on some message")
 				return
 
-		await message.bot.delete_message(chat_id=message.chat.id, message_id=message.message_id)
+		await message.bot.delete_message(chat_id=config.GROUP_ID, message_id=message.message_id)
 		await message.bot.kick_chat_member(
-			chat_id=message.chat.id,
+			chat_id=config.GROUP_ID,
 			user_id=message.reply_to_message.from_user.id,
 		)
 		await message.reply_to_message.reply("😈 User is kicked")
@@ -60,42 +61,64 @@ async def user_ban(message: types.Message):
 @dp.message_handler(is_admin=True, commands=["reborn"], commands_prefix="!/")
 async def user_unban(message: types.Message):
 		await message.bot.unban_chat_member(
-			chait_id=message.chat.id,
+			chait_id=config.GROUP_ID,
 			user_id=message,
 			only_if_banned=True
 		)
 		await message.reply_to_message.reply("😇 User is reborned")
 
-# show user's karma
-@dp.message_handler(is_admin=True, commands=["karma"], commands_prefix="!/")
+# show user's status
+@dp.message_handler(is_admin=True, commands=["status"], commands_prefix="!/")
 async def show_user_karma(message: types.Message):
 		user_karma = db.get_user_karma(message.from_user.id)
+		user_status = bot_helpers.get_status_by_karma(user_karma)
 
 		await message.bot.send_message(
-			message.chat.id,
-			f"📜 Your karma: {str(user_karma)}"
+			config.GROUP_ID,
+			f"📜 Your status: {str(user_status)}"
 		)
 
-# pin a message (admins only)
-@dp.message_handler(is_admin=True, commands=["pin"], commands_prefix="!/")
-async def pin_message(message: types.Message):
-		real_message = bot_helpers.message_without_command('/pin', message.text)
+		if int(user_karma) == -42:
+				await send_kicking_poll(
+					config.GROUP_ID,
+					message.from_user.first_name,
+					message.from_user.id,
+				)
 
-		if not real_message:
-				await message.reply("⚠️ Empty message")
-				return
+# send a poll about user kicking
+async def send_kicking_poll(*args):
+		[chat_id, user_name, user_id] = args
 
-		await message.bot.pin_chat_message(
-			message.chat.id,
-			message.message_id,
-			True, # disable notification for members
+		delay = 10
+		response = await bot.send_poll(
+			chat_id=chat_id,
+			question=f"Delete {user_name} from the group ❓",
+			options=["😒 Yes, we don't need them", "😇 No, he's one of us"],
+			is_anonymous=True,
+			type="regular",
+			open_period=delay,
 		)
-		await message.bot.send_message(message.chat.id, "👍 Message is pinned")	
 
-# unpin a last message (admins only)
-@dp.message_handler(is_admin=True, commands=["unpin"], commands_prefix="!/")
-async def unpin_message(message: types.Message):
-		await message.bot.unpin_chat_message(message.chat.id)
+		time.sleep(delay - 1)
+
+		poll_results = await bot.stop_poll(
+			chat_id=chat_id,
+			message_id=response.message_id,
+		)
+
+		[agree, disagree] = poll_results.options
+
+		if agree.voter_count > disagree.voter_count:
+				try:		
+						await bot.kick_chat_member(
+							chat_id=config.GROUP_ID,
+							user_id=user_id,
+						)
+						await bot.send_message("😈 User is kicked")
+				except:
+						await bot.send_message("The deletion failed. Isn't this the admin?")
+		else:
+				await bot.send_message("🤞 This time he was lucky")
 
 # secret method (admins only)
 @dp.message_handler(is_admin=True, commands=["secret"], commands_prefix="!/")
@@ -108,13 +131,13 @@ async def secret_method(message: types.Message):
 				fake_secret = "*" * len(user_secret)
 
 				await message.bot.send_message(
-					message.chat.id,
+					config.GROUP_ID,
 					f"❌ Wrong secret: {fake_secret}",
 				)
 				return
 
 		await message.bot.send_message(
-			message.chat.id,
+			config.GROUP_ID,
 			f"✨ Secret mode is activated for {message.chat.first_name}",
 		)
 
@@ -128,7 +151,7 @@ async def filter_messages(message: types.Message):
 
 				await message.delete()
 				await message.bot.send_message(
-					message.chat.id,
+					config.GROUP_ID,
 					f"👎 Inappropriate language\. Karma for *{first_name}* lowered",
 					"MarkdownV2",
 				)
