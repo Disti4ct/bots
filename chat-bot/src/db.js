5fb9e64c3a53536c2d45282f9e1a3e3cb0ca3ff1
NotEternal <cent1pede@protonmail.ch>
2023-04-12 17:16:04 +0300

    feat: add simple monetization

--- chat-bot/src/db.js
+++ chat-bot/src/db.js
@@ -22,3 +22,51 @@ export const countMessage = async (message, db) => {
     }
   }
 };
+
+export const needToAskForPayment = async ({
+  userId,
+  db,
+  amountOfFreeMessages,
+}) => {
+  const userData = await db.get(userId);
+
+  if (userData) {
+    const { messageCounter, isItPaidFor } = JSON.parse(userData);
+
+    if (isItPaidFor) return false;
+    if (messageCounter > amountOfFreeMessages) return true;
+  }
+
+  return false;
+};
+
+export const validateActivationMessage = async ({
+  message,
+  activationCode,
+  botToken,
+}) => {
+  if (message.text.match(/This is the activation code: ?\n?[a-z0-9]{32}$/m)) {
+    // Extract code sent
+    const codeSent = message.text.match(/[a-z0-9]{32}/);
+
+    // If code isn't right send a message about it
+    if (codeSent !== activationCode) {
+      await sendMessage("Your code is incorrect", botToken, message.chat.id);
+
+      return false;
+    }
+
+    // If it's correct we save info about it
+    // So he won't need to pay anymore
+    const userId = message.from.id;
+    const userData = await db.get(userId);
+
+    await db.put(
+      userId,
+      JSON.stringify({
+        ...userData,
+        isItPaidFor: true,
+      })
+    );
+  }
+};
--- chat-bot/src/index.js
+++ chat-bot/src/index.js
@@ -1,4 +1,4 @@
-import { countMessage } from "./db";
+import { countMessage, needToAskForPayment } from "./db";
 import { msgChatWithOpenAI } from "./openai";
 import { sendMessage, setupBot } from "./telegram";
 
@@ -28,25 +28,72 @@ const requestToOpenAI = async ({ openAiToken, text }) => {
 };
 
 const handleRequest = async (params) => {
-  const { request, botToken, db } = params;
+  const {
+    request,
+    botToken,
+    db,
+    activationCode,
+    amountOfFreeMessages,
+    paymentLink,
+  } = params;
   const { pathname } = new URL(request.url);
 
   try {
     if (pathname === `/telegram/${botToken}/webhook`) {
       const message = await extractUserMessage(request);
 
-      await countMessage(message, db);
-
       // Check if we have a text parameter, so ChatGPT can process it
       // and if we have a chat id so we send it back to a right user
       if (message?.text && message?.chat?.id) {
+        // First of all check the message format
+        // If a user sent an activation code we validate it
+        const validationResult = await validateActivationMessage({
+          message,
+          activationCode,
+          botToken,
+        });
+
+        if (validationResult === false) return;
+
+        // Check if the user has reached the limit of free messages
+        // Send a payment link and ask for an activation code
+        const isPaymentRequired = await needToAskForPayment({
+          userId: message.from.id,
+          db,
+          amountOfFreeMessages,
+        });
+
+        if (isPaymentRequired) {
+          await sendMessage(
+            `
+          <b>You've reached the limit of free messages.</b>
+          <br />
+          To continue using this bot you need to pay for the activation code via the link below:
+          <br />
+          <a href="${paymentLink}">Pay for usage</a>
+          <br />
+          After payment, you need to send a message here with an activation code in the format:
+          <br />
+          <b>This is the activation code:
+            <br />
+            af9e4f3ef2080a003ef910dc2575497d
+          </b>
+          `,
+            botToken,
+            message.chat.id
+          );
+          return;
+        }
+
+        await countMessage(message, db);
+
         const response = await requestToOpenAI({
           ...params,
-          text: message?.text,
+          text: message.text,
         });
 
         if (response) {
-          await sendMessage(response, botToken, message?.chat?.id);
+          await sendMessage(response, botToken, message.chat.id);
         } else if (response instanceof Error) {
           await sendMessage(
             "Cannot process your request",
@@ -79,7 +126,14 @@ const handleRequest = async (params) => {
 
 export default {
   async fetch(request, env) {
-    const { TG_BOT_TOKEN, OPEN_AI_TOKEN, DATABASE } = env;
+    const {
+      TG_BOT_TOKEN,
+      OPEN_AI_TOKEN,
+      DATABASE,
+      ACTIVATION_CODE,
+      AMOUNT_OF_FREE_MESSAGES,
+      LINK_TO_PAY_FOR_CODE,
+    } = env;
 
     await setupBot(request, TG_BOT_TOKEN);
 
@@ -88,6 +142,9 @@ export default {
       botToken: TG_BOT_TOKEN,
       openAiToken: OPEN_AI_TOKEN,
       db: DATABASE,
+      activationCode: ACTIVATION_CODE,
+      amountOfFreeMessages: AMOUNT_OF_FREE_MESSAGES,
+      paymentLink: LINK_TO_PAY_FOR_CODE,
     });
 
     return response || new Response("yep", { status: 200 });
